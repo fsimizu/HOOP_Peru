84556e24b68c9471b080991fad7e8a7d78c948a3
Fernando Simizu <AU00903405@prosegur.com>
2023-07-18 15:47:45 +1000

    fix

--- src/routes/volunteer.jsx
+++ src/routes/volunteer.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import { Footer } from "../components/Footer";
-import { Navbar } from "../components/Navbar";
 import ContentVolunteer from "../components/ContentVolunteer/ContentVolunteer";
-import { Volunteers } from "../components/Volunteers";
-import {HeroCommon} from "../components/HeroCommon"
-import HoopVolunteers from "../components/HoopVolunteers/HoopVolunteers";
-import { SocialMedia } from "../components/SocialMedia";
 import { GetInvolvedBtn } from "../components/GetInvolvedBtn";
+import { HeroCommon } from "../components/HeroCommon";
+import HoopVolunteers from "../components/HoopVolunteers/HoopVolunteers";
 import { Layout } from "../layout/Layout.jsx";
 
 
